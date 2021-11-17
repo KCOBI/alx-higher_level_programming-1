ea19eccd291408337bdec6515324d5548dae0668
robi000 <smwefyelem@gmail.com>
2021-11-17 11:35:46 +0300

    e

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *before, *new_Node;
+
+	new_Node = malloc(sizeof(listint_t));
+	if (new_Node != NULL)
+	{
+		new_Node->n = number;
+		if (*head == NULL || (*head)->n >= new_Node->n)
+		{
+			new_Node->next = *head;
+			*head = new_Node;
+			return (new_Node);
+		}
+		else
+		{
+			before = *head;
+			while (before->next != NULL && before->next->n < new_Node->n)
+				before = before->next;
+			new_Node->next = before->next;
+			before->next = new_Node;
+			return (new_Node);
+		}
+	}
+	return (NULL);
+}
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    last = ''
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            last += chr(ord(char)-32)
+        else:
+            last += char
+    print(last)
