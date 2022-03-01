49b763f16d50ea444a005d1739422cc9301fc071
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-01 21:02:16 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,8 +2,8 @@
 USE hbtn_0d_usa
 
 SELECT c.id, c.name, s.name
-FROM cities as c
-INNER JOIN states as s
+FROM cities AS c
+INNER JOIN states AS s
 ON s.id = c.state_id
-order by c.id
+ORDER BY c.id
 ;
