0fbc7d9a9fd8137ae93453944b18a47e4d76b7e0
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-01 19:36:15 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
 -- this will create database and also states which its id and name not null and also incriment
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY  UNIQUE , state_id INT NOT NULL , FOREIGN KEY(state_id) REFERENCES state(id) ,name VARCHAR(256) NOT NULL );
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY  UNIQUE , state_id INT NOT NULL , FOREIGN KEY(state_id) 
+                                   REFERENCES state(id) ,name VARCHAR(256) NOT NULL );
