4d9170988cafafd7477ca05de89b6e6614f3965a
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-01 20:34:44 +0300

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- this will display  all city found in californiya wht ever the id of californiya is
 
-USE hbtn_0d_usa;
 SELECT id , name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')  ORDER BY id;
