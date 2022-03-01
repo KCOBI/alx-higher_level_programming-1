5b7ba05e7c9423ab3cb5963a04056973f121d826
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-01 20:10:36 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,10 @@
--- this will create database and also states which its id and name not null and also incriment
+-- creates the database hbtn_0d_usa and the table cities
+-- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY  UNIQUE , state_id INT NOT NULL , FOREIGN KEY(state_id) 
-                                   REFERENCES state(id) ,name VARCHAR(256) NOT NULL );
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    CONSTRAINT FK_STATES_ID FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
