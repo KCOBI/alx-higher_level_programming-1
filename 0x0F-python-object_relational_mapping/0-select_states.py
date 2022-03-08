b7f7121f11206d0e0ec69f462f0ef6c7c6b1fc9a
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-08 18:21:47 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""this will select states from user his will select states from user his will select states from user
-    """
-import MySQLdb
+"""
+Module 0-select_states.py
+Takes 3 arguments: username, password and database name to connect to the mysql
+serve running on localhost port 3306.
+"""
+
 from sys import argv
+import MySQLdb
 
 
 def main():
-    """this will execute the main program or the select part which is what we want
     """
-    dbb = MySQLdb.connect(user=argv[1], port=3306, host='localhost',
-                          passwd=argv[2], db=argv[3])
-    c = dbb.cursor()
+    Program starts here. Lists all states from the database hbtn_0e_0_usa
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id")
     for i in c.fetchall():
         print(i)
+
 
 if __name__ == "__main__":
     main()
