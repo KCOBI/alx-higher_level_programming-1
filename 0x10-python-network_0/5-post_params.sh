ade349c55a09341a58043230d1cf57b20573e22a
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 01:27:49 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this will show the allowd responses
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -d "email=test%40gmail.com&subject=I+will+always+be+here+for+PLD" -X POST $1
