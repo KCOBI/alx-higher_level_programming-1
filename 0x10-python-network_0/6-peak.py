65ecc9f39e33707bf523559bae704c74cabc3ef7
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 01:45:46 +0300

    Update 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""this module will return the pick of given array
+
+    Returns:
+        single intiger or None: _description_
+    """
+
+
+def find_peak(given):
+    """this wil sort the list and return the pick
+
+    Args:
+        given (list): list to be sorted
+
+    Returns:
+        single intiger or none: pick
+    """
+
+    if len(given) == 0:
+        return None
+
+    res = sorted(given)[-1]
+    return res
