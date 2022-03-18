6a3cebd375efbb0d84ba88ad979f11bd1ed59c3a
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 17:53:58 +0300

    Update 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""use post methode to ask server with given parameter"""
+
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    url = argv[1]
+    data = {"email": argv[2]}
+    data = urlencode(data)
+    data = data.encode("UTF-8")
+    full_url = Request(url, data)
+
+    with urlopen(full_url) as f:
+        print(f.read().decode("UTF-8"))
