b79bb264caec1bbce8bda8fbaa7ef26ef81ca3be
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 16:06:09 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# this will add custom header
-curl -X GET $1 -H "X-School-User-Id: 98"
+# Takes in a URL as an argument, sends a GET request to the URL, and displays
+# the body of the response
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
