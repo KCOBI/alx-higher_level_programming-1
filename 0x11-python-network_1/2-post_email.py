5a48a6bab5d5d431660ca129910736f9abd9931e
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 13:43:51 +0300

    Update 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""this module will return the reque    st id of response"""
+
+from urllib.request import urlopen
+from sys import argv
+
+with urlopen(argv[1]) as data:
+    print(data.headers['X-Request-Id'])
