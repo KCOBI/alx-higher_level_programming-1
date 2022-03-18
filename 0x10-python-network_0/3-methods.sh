9a9d589e2ea74727051eaef192f7b9244aebbc0c
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 16:01:57 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this will show the allowd responses
-curl -X OPTIONS $1 -sI | grep -i Allow | awk '{print $2 $3 $4 $5}'
+curl -s -i -X OPTIONS $1 | grep -i Allow | awk '{print $2 $3 $4 $5}'
