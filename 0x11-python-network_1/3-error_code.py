ffb6930410624d37a4d01cdca016e0b10acff936
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-18 18:00:41 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-"""use post methode to ask server with given parameter"""
-
-from sys import argv
-from urllib.error import HTTPError
-from urllib.error import URLError
-from urllib.parse import urlencode
-from urllib.request import Request
-from urllib.request import urlopen
+"""
+Take in a URL, send a request to URL, and dispaly body of response decoded in
+utf-8. Manage urllib's error exceptions.
+"""
+import sys
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-    url = argv[1]
+    req = urllib.request.Request(sys.argv[1])
     try:
-        with urlopen(url) as f:
-            print(f.read().decode("utf-8"))
-    except HTTPError as e:
-        print("Error code:", e.code)
-    except URLError as e:
-        print(e.reason)
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
