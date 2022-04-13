000cd024fbb7fa5c06f987b3337c149831d1e8e3
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 02:28:47 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+const option = {
+  url: args[2],
+  method: 'GET'
+};
+
+request(option, function (err, res, body) {
+  if (!err) {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
