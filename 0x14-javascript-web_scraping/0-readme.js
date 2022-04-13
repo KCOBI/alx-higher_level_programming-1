04e7adf5ce942ae9d8b28c756d82964e9b8c96d6
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 02:05:13 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const fs = require ('fs');
-fs.readFile (args[2], 'utf-8', (err, data) => {
+const fs = require('fs');
+fs.readFile(args[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error (err);
+    console.error(err);
   }
-  console.log (data);
+  console.log(data);
 });
