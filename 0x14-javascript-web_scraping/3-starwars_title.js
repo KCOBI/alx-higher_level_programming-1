53f80a2fb79f5f66a0c8729194f19b0828ba7d12
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 02:43:46 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,12 +4,12 @@ const request = require('request');
 const args = process.argv;
 
 const option = {
-  url: args[2],
+  url: 'https://swapi-api.hbtn.io/api/films/' + args[2],
   method: 'GET'
 };
 
 request(option, function (err, res, body) {
   if (!err) {
-    console.log(`code: ${res.statusCode}`);
+    console.log(`${JSON.parse(body).title}`);
   }
 });
