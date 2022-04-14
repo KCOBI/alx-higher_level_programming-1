b9e98f45bc41b295052d8e00f3e9ad010c78ab8c
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 03:25:35 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+const option = {
+  url: args[2],
+  method: 'GET'
+};
+
+let meh = 0;
+request(option, function (err, res, body) {
+  if (!err) {
+    const jfile = JSON.parse(body);
+    // console.log (jfile.results);
+    jfile.results.forEach(element => {
+      if (
+        element.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
+      ) {
+        meh++;
+      }
+    });
+  }
+  console.log(meh);
+});
