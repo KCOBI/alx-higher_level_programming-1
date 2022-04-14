bbbb44918cd1441dff14d8090030531b3ef778dd
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 16:26:26 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+
+const option = {
+  url: args[2],
+  method: 'GET'
+};
+
+request(option, function (err, res, body) {
+  res.setEncoding('utf8');
+  if (!err) {
+    // console.log (`${res.body}`);
+  }
+  const fstat = require('fs');
+  fstat.writeFile(args[3], res.body, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
