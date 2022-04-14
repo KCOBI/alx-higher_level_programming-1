12c2bf0870104a348e8004384600059ef55c542b
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-14 16:01:03 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,46 @@
 #!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+const option = {
+  url: args[2],
+  method: 'GET'
+};
+
+request(option, function (err, res, body) {
+  if (!err) {
+    // console.log (res.statusCode);
+    // console.log (body);
+    // console.log (`${JSON.parse (body)[0]['userId']}`);
+  } else {
+    console.log(err);
+    return;
+  }
+  const bodyx = JSON.parse(body);
+  const userids = [];
+  const final = {};
+  bodyx.forEach(element => {
+    if (!userids.includes(element.userId)) {
+      userids.push(element.userId);
+    }
+  });
+  // console.log ('user_ids: ', user_ids);
+  userids.forEach(element => {
+    final[element.toString()] = 0;
+  });
+  //  console.log ('final: ', final);
+
+  bodyx.forEach(element => {
+    if (element.completed) {
+      final[element.userId.toString()]++;
+    }
+  });
+  console.log(final);
+
+  userids.forEach(element => {
+    if (final[element.toString()] === 0) {
+      delete final[element.toString()];
+    }
+  });
+});
